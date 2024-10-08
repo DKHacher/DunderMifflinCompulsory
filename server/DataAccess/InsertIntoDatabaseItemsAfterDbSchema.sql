4b01e64ee3b9a1b540f14809b8aa2921c673368e
DKHacher <146067965+DKHacher@users.noreply.github.com>
2024-10-08 10:11:31 +0200

    added a small sql schema to insert some basic test data into the database

---
+++ server/DataAccess/InsertIntoDatabaseItemsAfterDbSchema.sql
@@ -0,0 +1,3 @@
+﻿Insert Into properties (property_name) values('Basic'), ('Sturdy'), ( 'Water Resistant'), ( 'Ink Resistant'), ( 'Colored: Yellow'), ( 'Size:A5'), ( 'Size:A3');
+Insert Into paper (name, discontinued, stock, price) values ('Steel Plate', true, 0, 439.99), ('A4', false,150,25.99);
+insert into paper_properties values (1, 2), (1, 3), (1,4), (2,1);
